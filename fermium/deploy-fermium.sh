170998ceb53880de2240ac557c26f7d4c7523374
krallice <sroz9960@gmail.com>
2021-09-28 16:46:13 +1000

    adds log messages to fermium deploy

--- fermium/deploy-fermium.sh
+++ fermium/deploy-fermium.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Install Deps:
+echo "Installing Deps" | systemd-cat -t deploy-fermium
 apt update
 apt install python3.8-venv
 
 # Install Python Environment:
+echo "Creating Python Environment" | systemd-cat -t deploy-fermium
 mkdir -v /fermium
 cd /fermium
 python3 -m venv ./venv
 . ./venv/bin/activate
 
 # Grab Code:
+echo "Grabbing Code" | systemd-cat -t deploy-fermium
 curl https://raw.githubusercontent.com/krallice/config/master/fermium/main.py -o ./main.py
 
 # Run:
+echo "Running Fermium" | systemd-cat -t deploy-fermium
 export FLASK_APP="main"; flask run --host="0.0.0.0" --port="80"
